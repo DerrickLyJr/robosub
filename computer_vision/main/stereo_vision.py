cc16150d7d1b4c4396333b56aa1f64b2283abd01
Jesus Betancourt <jbeta02@gmail.com>
2026-01-05 23:19:46 -0800

    seperate stereo_vision into functions

--- computer_vision/main/stereo_vision.py
+++ computer_vision/main/stereo_vision.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from computer_vision.main import image_capture
 from computer_vision import config
 
+# with current parameters give acc of +/- 20 cm. there is still a decent amount of block spots that affect readings
+
 cap0, cap1 = image_capture.camera_setup(config.LEFT_CAM_ID, config.RIGHT_CAM_ID)
 
 # load calibration data
@@ -22,7 +24,7 @@ P2 = data["P2"]
 Q = data["Q"]
 
 
-while True:
+def get_disparity(display):
     ret0, frame0 = cap0.read()
     ret1, frame1 = cap1.read()
     frame0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
@@ -39,7 +41,7 @@ while True:
     )
 
     frame0 = cv2.remap(frame0,  map1x, map1y, cv2.INTER_LINEAR)
-    frame1 = cv2.remap(frame1,  map1x, map1y, cv2.INTER_LINEAR)
+    frame1 = cv2.remap(frame1,  map2x, map2y, cv2.INTER_LINEAR)
 
     # grab values found with parameter_tuning.py
     channels = 1 # since using grayscale use 1
@@ -65,40 +67,69 @@ while True:
 
     map = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
-    cv2.line(map,(map.shape[1]//2,0), (map.shape[1]//2, map.shape[0]), 255, 1)
-    cv2.line(map,(0,map.shape[0]//2), (map.shape[1], map.shape[0]//2), 255, 1)
+    if display:
+        cv2.line(map,(map.shape[1]//2,0), (map.shape[1]//2, map.shape[0]), 255, 1)
+        cv2.line(map,(0,map.shape[0]//2), (map.shape[1], map.shape[0]//2), 255, 1)
 
-    cv2.imshow(f"map: numDisp {num_disp}, blockSize {block_size}", map)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow(f"map: numDisp {num_disp}, blockSize {block_size}", map)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+        cv2.waitKey(1)
+
+    return map, disparity, Q
+
+
+def get_center_distance(display, window_square_size=50):
+    map, disparity, Q = get_disparity(display)
 
     points_3D = cv2.reprojectImageTo3D(disparity, Q)
-    X, Y, Z = points_3D[imageSize[0]//2, imageSize[1]//2]   # y first, then x
 
-    size = 50//2
+    size = window_square_size//2
     center = map.shape[0]//2, map.shape[1]//2
     depth = points_3D[..., 2] # get only depth (Z)
     valid = np.isfinite(depth) & (depth > 0)
     valid_block = valid[center[0]-size:center[0]+size, center[1]-size:center[1]+size]
     depth_block = depth[center[0]-size:center[0]+size, center[1]-size:center[1]+size]
     center_depth = depth_block[valid_block]
-    print("depth at center in meters: ", np.mean(center_depth)) ###################################### with current parameters give acc of +/- 20 cm. there is still a decent amount of block spots that affect readings
+    print("depth at center in meters: ", np.mean(center_depth))
+    center_depth_val = np.mean(center_depth)
+    return center_depth_val
 
-    # Get indices of valid depths
+
+def get_closest_obj_distance(display):
+    map, disparity, Q = get_disparity(display)
+
+    points_3D = cv2.reprojectImageTo3D(disparity, Q)
+
+    depth = points_3D[..., 2] # get only depth (Z)
+    valid = np.isfinite(depth) & (depth > 0)
+
+    # get indices of valid depths
     ys, xs = np.where(valid)
-    # Get depths of valid points
+    # get depths of valid points
     valid_depths = depth[ys, xs]
 
-    # Index of closest point among valid
+    # index of closest point among valid
     i = np.argmin(valid_depths)
 
-    # Pixel coordinate of closest object
+    # pixel coordinate of closest object
     y = ys[i]
     x = xs[i]
 
-    # World coords
+    # world coords
     X, Y, Z = points_3D[y, x]
     print(f"pixel coord x, y: {x}, {y}")
     print(f"x, y, z: {X}, {Y}, {Z} in meters")
 
-cv2.destroyAllWindows()
+    return X, Y, Z, x, y # meter coordinates X, Y, X, pixel coordinates x, y
+
+
+if __name__ == "__main__":
+
+    display = True
+
+    while True:
+        get_center_distance(display, 25)
+
+    if display:
+        cv2.destroyAllWindows()
